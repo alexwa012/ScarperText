9fa24b6aa8f36e8c77ca6b14eabd43188348332a
alexwa012 <alexwa012@gmail.com>
2025-08-23 12:07:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Install deps first:
 // npm install express axios node-html-parser cors dotenv firebase-admin xml2js crypto
+//Deploy
 
 const express = require("express");
 const axios = require("axios");
@@ -242,4 +243,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
 
